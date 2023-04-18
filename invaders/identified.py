58609fbbed48a87c44d04e0a3142863d9116ce2e
Iulian Gulea <iulian.gulea@gmail.com>
2023-04-18 22:49:23 +0300

    Remove unnecessary __init__

--- invaders/identified.py
+++ invaders/identified.py
@@ -1,15 +1,11 @@
 from core.mixins import BinaryToAsciiMixin
-from core.types import Frame
-from invaders.base import Invader, IdentifiedInvader
+from invaders.base import IdentifiedInvader
 
 
 class AsciiIdentifiedInvader(BinaryToAsciiMixin, IdentifiedInvader):
     """
     An identified Invader that can be represented in ASCII.
     """
-    def __init__(self, original: Invader, binary_matrix: Frame, similarity_ratio: float, frame_coords_on_map: list[int]):
-        super().__init__(original, binary_matrix, similarity_ratio, frame_coords_on_map)
-
     def pretty_representation(self):
         invader_string = self.convert_binary_matrix_to_ascii(self.pattern)
 
